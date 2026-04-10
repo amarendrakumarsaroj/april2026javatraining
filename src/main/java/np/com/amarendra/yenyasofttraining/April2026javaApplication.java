2c3b639f6bb157706c0ab8b68b520927c38e9a7e
Amarendra Kumar Saroj <dominonepal@gmail.com>
2026-04-10 16:48:05 +0545

    SETUP : Initial setup complete with yml configurations and pgsql connection

---
+++ src/main/java/np/com/amarendra/yenyasofttraining/April2026javaApplication.java
@@ -0,0 +1,13 @@
+package np.com.amarendra.yenyasofttraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class April2026javaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(April2026javaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/np/com/amarendra/yenyasofttraining/April2026javaApplicationTests.java
@@ -0,0 +1,13 @@
+package np.com.amarendra.yenyasofttraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class April2026javaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
